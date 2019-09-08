96a4f698f6bf7ef9129211d3c11bb74ebdbb6733
Gabe Gutierrez <gabedotgutierrez@gmail.com>
2019-09-08 21:23:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
-answers = []
 
 def find_element_index(array, value_to_find)
        array.length.times { |index|
+         answers = []
        if value_to_find == array[index]
          answers << index
        end
